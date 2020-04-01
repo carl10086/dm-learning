73f64cf8b5af2679765c9c07ce0ada58f2f423f2
carl_yu <yszlx200@163.com>
2020-04-01 19:06:05 +0800

    bytebuddy 功能集成

---
+++ dm-bytebuddy/src/main/java/com/ysz/dm/bytebuddy/first/BasicDm_MethodDelegation_002.java
@@ -0,0 +1,14 @@
+package com.ysz.dm.bytebuddy.first;
+
+/**
+ * @author carl
+ */
+public class BasicDm_MethodDelegation_002 {
+
+
+
+
+  public static void main(String[] args) throws Exception {
+
+  }
+}
