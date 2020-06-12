7e328e6fbd2c8da6192a21880df2eed26151b2ef
carl_yu <sizhen.yu@duitang.com>
2020-06-12 17:28:21 +0800

    change: 容忍域名格式部分错误
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

---
+++ dm-fast/src/main/java/com/ysz/dm/fast/basic/Npl_001.java
@@ -0,0 +1,25 @@
+package com.ysz.dm.fast.basic;
+
+import org.apache.commons.io.FileUtils;
+
+public class Npl_001 {
+
+  private Long id;
+
+  public Long getId() {
+    return this.id;
+  }
+
+  public Npl_001 setId(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  public static void main(String[] args) {
+    Long id = null;
+    Long val = (id == null) ? 0L : null;
+  }
+
+
+
+}
