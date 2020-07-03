1d43ab28941e8ac0b7e504b02ad8f4b05d76a889
carl_yu <sizhen.yu@duitang.com>
2020-07-03 19:13:56 +0800

    version 1
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

---
+++ dm-biz/src/main/java/com/ysz/biz/validator/Validate_Dm_001.java
@@ -0,0 +1,41 @@
+package com.ysz.biz.validator;
+
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import javax.validation.metadata.ConstraintDescriptor;
+import org.hibernate.validator.HibernateValidator;
+
+/**
+ * @author carl
+ */
+public class Validate_Dm_001 {
+
+  private ValidatorFactory factory;
+
+  private Validator validator;
+
+
+  public void validateComplexBean() {
+
+    factory = Validation.byProvider(HibernateValidator.class)
+        .configure()
+        // 快速失败模式
+        .failFast(true)
+        // .addProperty( "hibernate.validator.fail_fast", "true" )
+        .buildValidatorFactory();
+
+    validator = factory.getValidator();
+    Set<ConstraintViolation<Object>> validateRes = validator.validate(ComplexBean.mock());
+    for (ConstraintViolation<Object> validateRe : validateRes) {
+      System.out.println(validateRe.getMessage());
+    }
+  }
+
+  public static void main(String[] args) {
+    new Validate_Dm_001().validateComplexBean();
+  }
+
+}
