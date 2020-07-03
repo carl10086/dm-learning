a3065f19898ca0d63ad1607602b1d22d5b77ffc9
carl_yu <yszlx200@163.com>
2020-07-03 16:20:41 +0800

    version 1
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

--- dm-fast/src/main/java/com/ysz/dm/fast/basic/Npl_001.java
+++ dm-fast/src/main/java/com/ysz/dm/fast/basic/Npl_001.java
@@ -1,25 +1,48 @@
 package com.ysz.dm.fast.basic;
 
-import org.apache.commons.io.FileUtils;
+import com.google.common.collect.Lists;
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.time.FastDateFormat;
 
 public class Npl_001 {
 
-  private Long id;
+  private static long start;
+  private static final long UNIT = 60L * 3600L * 1000L;
 
-  public Long getId() {
-    return this.id;
+  {
+    try {
+      start = FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss").parse("2014-06-15 10:00:00")
+          .getTime();
+    } catch (ParseException e) {
+    }
   }
 
-  public Npl_001 setId(Long id) {
-    this.id = id;
-    return this;
+
+  private static float score(int vote, Date createAt) {
+    long t = createAt.getTime() - start;
+    float rightPart = new BigDecimal(t).divide(new BigDecimal(UNIT), 4, BigDecimal.ROUND_CEILING)
+        .floatValue();
+    if (vote == 0) {
+      return rightPart;
+    }
+    float leftPart = (float) (Math.log(vote) / Math.log(2));
+    return leftPart + rightPart;
   }
 
-  public static void main(String[] args) {
-    Long id = null;
-    Long val = (id == null) ? 0L : null;
-  }
+  public static void main(String[] args) throws Exception {
+    /*1 在*/
+    List<String> data = Lists.newArrayList("1", "2", "3", "4");
 
+    /*4个 -> yarn 调度器 ... yarn -> master -> job 2 _> 2*/
+
+    data.stream().map(x -> x + "a").filter(Objects::nonNull).sorted().collect(Collectors.toList());
+
+  }
 
 
 }
