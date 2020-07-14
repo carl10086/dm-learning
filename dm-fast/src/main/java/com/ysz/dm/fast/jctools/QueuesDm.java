0262ae77689d5f699fbc7e9a2dfddf68b545d108
carl_yu <yszlx200@163.com>
2020-07-14 18:59:27 +0800

    blog v2 refactor
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

---
+++ dm-fast/src/main/java/com/ysz/dm/fast/jctools/QueuesDm.java
@@ -0,0 +1,20 @@
+package com.ysz.dm.fast.jctools;
+
+import com.google.common.collect.Queues;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class QueuesDm {
+
+  public static void main(String[] args) throws Exception {
+    ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(100);
+    for (int i = 0; i < 10; i++) {
+      queue.offer(i);
+
+      List<Integer> buffer = new ArrayList<>(10);
+      Queues.drain(queue, buffer, 10, Duration.ofSeconds(10));
+    }
+  }
+}
