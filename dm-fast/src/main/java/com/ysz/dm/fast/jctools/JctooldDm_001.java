3fd21b57d1f43ea4a07e9ebcf9d953e6c0cd43c7
carl_yu <yszlx200@163.com>
2020-07-14 18:52:59 +0800

    blog v2 refactor
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

---
+++ dm-fast/src/main/java/com/ysz/dm/fast/jctools/JctooldDm_001.java
@@ -0,0 +1,22 @@
+package com.ysz.dm.fast.jctools;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.jctools.queues.SpscArrayQueue;
+
+public class JctooldDm_001 {
+
+  public static void main(String[] args) {
+    SpscArrayQueue<Integer> queue = new SpscArrayQueue<>(10);
+    for (int i = 0; i < 10; i++) {
+      System.out.println(queue.offer(i));
+    }
+
+    List<Integer> buffer = new ArrayList<>();
+    queue.drain(buffer::add);
+    for (Integer i : buffer) {
+      System.out.println(i);
+
+    }
+  }
+}
