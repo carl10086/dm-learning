686b07834d6b0a3e1f98cc111d4a0638dc0ad08f
carl_yu <yszlx200@163.com>
2020-07-24 18:36:43 +0800

    change spark trainer
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

---
+++ dm-bytebuddy/src/main/java/com/ysz/dm/bytebuddy/jvm/Method_Dm_001.java
@@ -0,0 +1,19 @@
+package com.ysz.dm.bytebuddy.jvm;
+
+/**
+ * @author carl
+ */
+public class Method_Dm_001 {
+
+  public void test() {
+    DmBeanImpl dmBean = new DmBeanImpl();
+    dmBean.test();
+  }
+
+  public static void main(String[] args) {
+    new Method_Dm_001().test();
+  }
+
+
+}
+
