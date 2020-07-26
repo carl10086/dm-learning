f4ffc13f663042df5d47c7bd20e91962163eab34
carl.yu <carl.yu@carlyudeMacBook-Pro.local>
2020-07-27 01:22:51 +0800

    dm netty
    Signed-off-by: carl.yu <carl.yu@carlyudeMacBook-Pro.local>

---
+++ dm-netty/src/main/java/com/ysz/dm/netty/echo/EchoServer.java
@@ -0,0 +1,60 @@
+package com.ysz.dm.netty.echo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class EchoServer {
+
+  public static void main(String[] args) throws Exception {
+    EventLoopGroup boss = new NioEventLoopGroup(1);
+    EventLoopGroup work = new NioEventLoopGroup();
+
+    try {
+      ServerBootstrap serverBootstrap = new ServerBootstrap();
+      serverBootstrap
+          .group(boss, work)
+          .channel(NioServerSocketChannel.class)
+          .option(ChannelOption.SO_BACKLOG, 5)
+          .childHandler(
+              new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                  ch.pipeline()
+                      .addLast(
+                          new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg)
+                                throws Exception {
+                              ctx.write(msg);
+                            }
+
+                            @Override
+                            public void channelReadComplete(ChannelHandlerContext ctx)
+                                throws Exception {
+                              ctx.flush();
+                            }
+
+                            @Override
+                            public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+                                throws Exception {
+                              cause.printStackTrace();
+                              ctx.close();
+                            }
+                          });
+                }
+              });
+
+      ChannelFuture channelFuture = serverBootstrap.bind(1234).sync();
+      // Wait until the server socket is closed.
+      channelFuture.channel().closeFuture().sync();
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      boss.shutdownGracefully();
+      work.shutdownGracefully();
+    }
+  }
+}
