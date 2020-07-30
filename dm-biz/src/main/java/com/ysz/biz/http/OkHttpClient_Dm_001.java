22a7d39300e39db6932c38ea953f33a91d4c426c
carl_yu <yszlx200@163.com>
2020-07-30 18:49:08 +0800

    ok http client
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

--- dm-biz/src/main/java/com/ysz/biz/http/OkHttpClient_Dm_001.java
+++ dm-biz/src/main/java/com/ysz/biz/http/OkHttpClient_Dm_001.java
@@ -1,40 +1,84 @@
 package com.ysz.biz.http;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.nio.charset.Charset;
-import okhttp3.Call;
-import okhttp3.MediaType;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import org.apache.commons.io.IOUtils;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author carl
  */
 public class OkHttpClient_Dm_001 {
 
-  private ObjectMapper objectMapper = new ObjectMapper();
+  private final ObjectMapper objectMapper = new ObjectMapper();
 
-  public OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+  public final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+      .addInterceptor(new LoggingInterceptor())
+      .readTimeout(10L, TimeUnit.MILLISECONDS)
+      .build();
 
-  public void post() throws Exception {
-    String s = objectMapper.writeValueAsString(new GelfHttpData());
-    System.out.println(s);
+  public void tstGet() {
 
-    final RequestBody requestBody = RequestBody
-        .create(s.getBytes(Charset.defaultCharset()), MediaType.parse("application/json"));
-    final Request request = new Request.Builder().url("http://10.1.2.5:12201/gelf")
-        .post(requestBody)
+    final Request request = new Request.Builder()
+        .url("https://c-ssl.duitang.com/uploads/item/202005/14/20200514123116_ctfff.png")
         .build();
-    final Call call = okHttpClient.newCall(request);
-    String s1 = call.execute().toString();
-    System.out.println(s1);
 
+    try (Response response = okHttpClient.newBuilder().readTimeout(200L, TimeUnit.MILLISECONDS)
+        .build()
+        .newCall(request).execute()) {
+      if (!response.isSuccessful()) {
+        System.err.println("请求失败!");
+        return;
+      }
+
+      final ResponseBody body = response.body();
+      assert body != null;
+      System.out.println("请求的长度是:" + body.contentLength());
+      try (FileOutputStream outputStream = new FileOutputStream("/Users/carl/tmp/image/1.png")) {
+        IOUtils.copyLarge(body.byteStream(), outputStream);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
 
   public static void main(String[] args) throws Exception {
-    new OkHttpClient_Dm_001().post();
+    new OkHttpClient_Dm_001().tstGet();
   }
 
+
+  @Slf4j
+  private static class LoggingInterceptor implements Interceptor {
+
+    @NotNull
+    @Override
+    public Response intercept(@NotNull Chain chain) throws IOException {
+      Request request = chain.request();
+
+      long t1 = System.nanoTime();
+      final String url = request.url().toString();
+      log.info(String
+          .format("Sending request %s on %s%n%s", url, chain.connection(), request.headers()));
+      try {
+        return chain.proceed(request);
+      } catch (Exception e) {
+        System.out.println("吃掉异常");
+        throw e;
+      } finally {
+        long t2 = System.nanoTime();
+        log.info(String.format("Received response for %s in %.1fms%n", url, (t2 - t1) / 1e6d));
+      }
+    }
+  }
 }
