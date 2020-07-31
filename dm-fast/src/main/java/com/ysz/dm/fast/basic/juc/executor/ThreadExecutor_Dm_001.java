003292a2fcc19d985bd3e0da8764febd6663b0c1
carl_yu <yszlx200@163.com>
2020-07-31 19:07:35 +0800

    change user 4000
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

---
+++ dm-fast/src/main/java/com/ysz/dm/fast/basic/juc/executor/ThreadExecutor_Dm_001.java
@@ -0,0 +1,51 @@
+package com.ysz.dm.fast.basic.juc.executor;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author carl
+ */
+public class ThreadExecutor_Dm_001 {
+
+  public static void main(String[] args) throws Exception {
+    final ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+        1,
+        4,
+        10L,
+        TimeUnit.MINUTES,
+        new ArrayBlockingQueue<>(3),
+        new ThreadFactoryBuilder().setNameFormat("testPool" + "-$d").build(),
+        new RejectedExecutionHandler() {
+          @Override
+          public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+            System.err.printf("线程打印拒绝日志:%s -> %s", Thread.currentThread().getName(), r.toString());
+          }
+        }
+    );
+
+    Thread thread = new Thread(new ThreadExecutorMonitor(threadPoolExecutor));
+    thread.setDaemon(true);
+    thread.start();
+
+    for (int j = 0; j < 10; j++) {
+      threadPoolExecutor.execute(() -> {
+        /*这里暂时无所谓、一个类死循环*/
+        for (int i = 0; i < 100; i++) {
+          try {
+            Thread.sleep(1000L);
+          } catch (InterruptedException ignored) {
+          }
+        }
+      });
+      /*每隔2s 加入一个任务*/
+      Thread.sleep(2000L);
+    }
+
+    threadPoolExecutor.shutdown();
+  }
+
+}
