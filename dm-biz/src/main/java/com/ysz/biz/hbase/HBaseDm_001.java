98f210b1adf36b7a83e50f8a716f2bae68c92625
carl_yu <yszlx200@163.com>
2020-10-15 12:23:20 +0800

    change cfg from 2->3
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

--- dm-biz/src/main/java/com/ysz/biz/hbase/HBaseDm_001.java
+++ dm-biz/src/main/java/com/ysz/biz/hbase/HBaseDm_001.java
@@ -3,14 +3,10 @@ package com.ysz.biz.hbase;
 import static org.apache.hadoop.hbase.util.Bytes.toBytes;
 import static org.apache.hadoop.hbase.util.Bytes.toInt;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.Row;
 import org.apache.hadoop.hbase.client.Table;
 
 public class HBaseDm_001 {
@@ -20,27 +16,17 @@ public class HBaseDm_001 {
     hBaseConnectionBean.setHbaseZk("10.1.3.52,10.1.3.32,10.1.3.51");
     hBaseConnectionBean.afterPropertiesSet();
 
+    final String normalizedBlogId = HBaseConnectionBean.normalize("1006890568");
+    System.err.println(normalizedBlogId);
     try (Connection connection = hBaseConnectionBean.getConnection()) {
 
-      try (Table table = connection.getTable(TableName.valueOf("message_message"))) {
-        List<Row> batch = new ArrayList<>();
-        final String normalizedBlogId = HBaseConnectionBean.normalize("1234");
+      try (Table table = connection.getTable(TableName.valueOf("mblog_like_counter"))) {
         final byte[] rowkey = toBytes(normalizedBlogId);
-
-        Put put = new Put(rowkey);
         final byte[] columnFam = toBytes("c");
-        put.addColumn(columnFam, toBytes("status"), toBytes(0));
-        batch.add(put);
-        Object[] resArray = new Object[batch.size()];
-        table.batch(batch, resArray);
-
         Get get = new Get(rowkey);
         Result result = table.get(get);
-        byte[] rootId = result.getValue(columnFam, toBytes("rootId"));
-        System.out.println(toInt(rootId));
-
-        byte[] status = result.getValue(columnFam, toBytes("status"));
-        System.out.println(toInt(status));
+        System.err.println(toInt(result.getValue(columnFam, toBytes("likeCnt"))));
+//        System.err.println(FastDateFormat.getInstance() .format(toLong(result.getValue(columnFam, toBytes("favAt")))));
       }
     }
 
