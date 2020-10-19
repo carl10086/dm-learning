c52b736ed5da791800540c3e23303a578d22e7f5
carl_yu <sizhen.yu@duitang.com>
2020-10-19 19:24:37 +0800

    tst kafka msg
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

--- dm-fast/src/main/java/com/ysz/dm/fast/util/FastUtilsDm.java
+++ dm-fast/src/main/java/com/ysz/dm/fast/util/FastUtilsDm.java
@@ -1,13 +1,12 @@
 package com.ysz.dm.fast.util;
 
-import org.apache.commons.lang3.time.FastDateFormat;
-import org.junit.Test;
-
 public class FastUtilsDm {
 
-  @Test
-  public void tstBigIntSet() throws Exception {
-    System.out.println(FastDateFormat.getInstance("yyyyMMdd HH").parse("20200630 12").getTime());
-  }
+//  @Test
+//  public void tstBigIntSet() throws Exception {
+//    System.out.println(FastDateFormat.getInstance("yyyyMMdd HH").parse("20200630 12").getTime());
+//  }
+
+
 
 }
