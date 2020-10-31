d1c8ffe910972456b4ebcab49d4e94ab78ce0101
俞思真 <carl.yu@yusizhendeMacBook-Pro.local>
2020-10-31 19:05:33 +0800

    fix ablum_event_0 -> album_event_1
    Signed-off-by: 俞思真 <carl.yu@yusizhendeMacBook-Pro.local>

--- dm-fast/src/main/java/com/ysz/dm/fast/algorithm/ArrayDm.java
+++
@@ -1,9 +0,0 @@
-package com.ysz.dm.fast.algorithm;
-
-public class ArrayDm {
-
-  public static void main(String[] args) {
-    
-  }
-
-}
---
+++ dm-fast/src/main/java/com/ysz/dm/fast/algorithm/linked/ListNode.java
@@ -0,0 +1,20 @@
+package com.ysz.dm.fast.algorithm.linked;
+
+public class ListNode {
+
+  int val;
+  ListNode next;
+
+  public ListNode(int x) {
+    val = x;
+  }
+
+  ListNode() {
+  }
+
+
+  public ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
