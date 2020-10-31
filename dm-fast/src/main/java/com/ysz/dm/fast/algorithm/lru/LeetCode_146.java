fcde32bc005feff8021c6b43669b103564dec97c
俞思真 <carl.yu@yusizhendeMacBook-Pro.local>
2020-11-01 00:01:15 +0800

    lru cache implementation
    Signed-off-by: 俞思真 <carl.yu@yusizhendeMacBook-Pro.local>

---
+++ dm-fast/src/main/java/com/ysz/dm/fast/algorithm/lru/LeetCode_146.java
@@ -0,0 +1,11 @@
+package com.ysz.dm.fast.algorithm.lru;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ * https://leetcode-cn.com/problems/lru-cache/
+ */
+public class LeetCode_146 {
+
+}
