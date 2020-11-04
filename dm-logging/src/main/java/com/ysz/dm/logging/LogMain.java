0debce20bd56f35564aa8c70b2d978b6ea0a5b16
俞思真 <carl.yu@yusizhendeMacBook-Pro.local>
2020-11-04 21:36:20 +0800

    dm logginggst
    Signed-off-by: 俞思真 <carl.yu@yusizhendeMacBook-Pro.local>

--- dm-logging/src/main/java/com/ysz/dm/logging/LogMain.java
+++ dm-logging/src/main/java/com/ysz/dm/logging/LogMain.java
@@ -3,8 +3,10 @@ package com.ysz.dm.logging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "file:///Users/carl.yu/tmp/useless/application.properties")
 public class LogMain {
 
   public static void main(String[] args) throws Exception {
