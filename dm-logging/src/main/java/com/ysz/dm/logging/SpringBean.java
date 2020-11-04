b5128971394559c71891ea8f7d366f027362834f
俞思真 <carl.yu@yusizhendeMacBook-Pro.local>
2020-11-04 21:36:20 +0800

    dm logginggst
    Signed-off-by: 俞思真 <carl.yu@yusizhendeMacBook-Pro.local>

--- dm-logging/src/main/java/com/ysz/dm/logging/SpringBean.java
+++ dm-logging/src/main/java/com/ysz/dm/logging/SpringBean.java
@@ -1,13 +1,18 @@
 package com.ysz.dm.logging;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class SpringBean {
 
+  @Value("${spring.profiles.active}")
+  private String profile;
+
   public void hello() {
+    System.err.println("profile:" + profile);
     log.error("hello world");
   }
 
