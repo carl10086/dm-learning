8e67000b9b7a28971eb6899d9dde16d23236b1e5
carl_yu <sizhen.yu@duitang.com>
2020-11-06 17:43:27 +0800

    jcol ...
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

--- dm-fast/src/main/java/com/ysz/dm/fast/basic/juc/cas/ConcurrentStack.java
+++ dm-fast/src/main/java/com/ysz/dm/fast/basic/juc/cas/ConcurrentStack.java
@@ -1,2 +1,49 @@
-package com.ysz.dm.fast.basic.juc.cas;public class ConcurrentStack {
+package com.ysz.dm.fast.basic.juc.cas;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ConcurrentStack<E> {
+
+  private AtomicReference<Node<E>> top = new AtomicReference<>();
+
+  private static class Node<E> {
+
+    public E item;
+    public Node<E> next;
+
+    public Node() {
+    }
+
+    public Node(final E item) {
+      this.item = item;
+    }
+  }
+
+  /**
+   * @desc: stack 并发争夺在于 top
+   *  1.push 只要替换 top
+   */
+  public void push(E item) {
+    Node<E> newHead = new Node<E>(item);
+    Node<E> oldHead;
+
+    do {
+      oldHead = top.get();
+      newHead.next = oldHead;
+    } while (!top.compareAndSet(oldHead, newHead));
+  }
+
+  public E pop() {
+    Node<E> oldHead;
+    Node<E> newHead;
+    do {
+      oldHead = top.get();
+      if (oldHead == null) {
+        return null;
+      }
+      newHead = oldHead.next;
+    } while (!top.compareAndSet(oldHead, newHead));
+
+    return oldHead.item;
+  }
 }
