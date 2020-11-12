ad06a5f4a9e612a4863aacd1a9de19fc7a2815cf
carl_yu <yszlx200@163.com>
2020-11-12 16:36:24 +0800

    debug fast util
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

---
+++ dm-fast/src/main/java/com/ysz/dm/fast/fastutil/BigArray_Dm_001.java
@@ -0,0 +1,20 @@
+package com.ysz.dm.fast.fastutil;
+
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
+
+public class BigArray_Dm_001 {
+
+
+  public static void main(String[] args) {
+
+    IntOpenHashSet set = new IntOpenHashSet(3);
+    set.add(1);
+    set.add(2);
+    set.add(3);
+
+    System.err.println(set
+    );
+
+  }
+
+}
