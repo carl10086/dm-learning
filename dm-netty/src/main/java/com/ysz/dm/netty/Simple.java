78268ba5149582295a57cf9c18279dfc998c8718
carl_yu <sizhen.yu@duitang.com>
2020-11-18 19:11:46 +0800

    misc.prop.tplv2
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

---
+++ dm-netty/src/main/java/com/ysz/dm/netty/Simple.java
@@ -0,0 +1,9 @@
+package com.ysz.dm.netty;
+
+public class Simple {
+
+  public static void main(String[] args) {
+    System.err.println(System.getProperty("username", "aa"));
+  }
+
+}
--- dm-netty/src/main/java/com/ysz/dm/netty/sofa/echo/RpcClientDemoByMain.java
+++ dm-netty/src/main/java/com/ysz/dm/netty/sofa/echo/RpcClientDemoByMain.java
@@ -1,5 +1,6 @@
 package com.ysz.dm.netty.sofa.echo;
 
+import com.alipay.remoting.InvokeContext;
 import com.alipay.remoting.exception.RemotingException;
 import com.alipay.remoting.rpc.RpcClient;
 
@@ -10,8 +11,9 @@ public class RpcClientDemoByMain {
     rpcClient.init();
     final String addr = "127.0.0.1:1234";
 
-    final Object hello = rpcClient.invokeSync(addr, new SimpleStringWrapper("hello"), 3000);
+    InvokeContext context = new InvokeContext();
+    final Object hello = rpcClient
+        .invokeSync(addr, new SimpleStringWrapper("hello"), context, 3000);
     System.err.println(hello);
   }
-
 }
