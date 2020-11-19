b7b7b53c7ae8fc0cd8419c34df300b4667d16e63
carl_yu <sizhen.yu@duitang.com>
2020-11-19 19:45:40 +0800

    a.ssl -> c.ssl
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

--- dm-netty/src/main/java/com/ysz/dm/netty/sofa/echo/RpcClientDemoByMain.java
+++ dm-netty/src/main/java/com/ysz/dm/netty/sofa/echo/RpcClientDemoByMain.java
@@ -3,6 +3,7 @@ package com.ysz.dm.netty.sofa.echo;
 import com.alipay.remoting.InvokeContext;
 import com.alipay.remoting.exception.RemotingException;
 import com.alipay.remoting.rpc.RpcClient;
+import com.alipay.remoting.rpc.RpcResponseFuture;
 
 public class RpcClientDemoByMain {
 
@@ -12,8 +13,11 @@ public class RpcClientDemoByMain {
     final String addr = "127.0.0.1:1234";
 
     InvokeContext context = new InvokeContext();
-    final Object hello = rpcClient
-        .invokeSync(addr, new SimpleStringWrapper("hello"), context, 3000);
-    System.err.println(hello);
+    final SimpleStringWrapper request = new SimpleStringWrapper("hello");
+    final int timeoutAsMs = 3000;
+    System.err.println(rpcClient.invokeSync(addr, request, context, timeoutAsMs));
+    final RpcResponseFuture responseFuture = rpcClient
+        .invokeWithFuture(addr, request, context, timeoutAsMs);
+    System.err.println(responseFuture.get());
   }
 }
