f7810ad59e94d9cdac8dbf034836da999e4ff8a0
carl_yu <sizhen.yu@duitang.com>
2020-11-24 15:26:40 +0800

    timer
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

--- dm-netty/src/main/java/com/ysz/dm/netty/sofa/echo/RpcClientDemoByMain.java
+++ dm-netty/src/main/java/com/ysz/dm/netty/sofa/echo/RpcClientDemoByMain.java
@@ -3,6 +3,7 @@ package com.ysz.dm.netty.sofa.echo;
 import com.alipay.remoting.InvokeContext;
 import com.alipay.remoting.exception.RemotingException;
 import com.alipay.remoting.rpc.RpcClient;
+import com.alipay.remoting.rpc.RpcResponseFuture;
 import java.io.IOException;
 
 public class RpcClientDemoByMain {
@@ -15,11 +16,11 @@ public class RpcClientDemoByMain {
 
     InvokeContext context = new InvokeContext();
     final SimpleStringWrapper request = new SimpleStringWrapper("hello");
-    final int timeoutAsMs = 3000;
-    System.err.println(rpcClient.invokeSync(addr, request, context, timeoutAsMs));
-    System.in.read();
-//    final RpcResponseFuture responseFuture = rpcClient
-//        .invokeWithFuture(addr, request, context, timeoutAsMs);
-//    System.err.println(responseFuture.get());
+    final int timeoutAsMs = 1000;
+//    System.err.println(rpcClient.invokeSync(addr, request, context, timeoutAsMs));
+//    System.in.read();
+    final RpcResponseFuture responseFuture = rpcClient
+        .invokeWithFuture(addr, request, context, timeoutAsMs);
+    System.err.println(responseFuture.get());
   }
 }
