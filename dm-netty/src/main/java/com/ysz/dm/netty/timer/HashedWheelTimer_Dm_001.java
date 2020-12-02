e0f6267cf869481dd30fec3e441fbd903f0f95fc
俞思真 <carl.yu@yusizhendeMacBook-Pro.local>
2020-12-02 23:23:41 +0800

    some test about hash wheelgst
    Signed-off-by: 俞思真 <carl.yu@yusizhendeMacBook-Pro.local>

--- dm-netty/src/main/java/com/ysz/dm/netty/timer/HashedWheelTimer_Dm_001.java
+++ dm-netty/src/main/java/com/ysz/dm/netty/timer/HashedWheelTimer_Dm_001.java
@@ -3,7 +3,9 @@ package com.ysz.dm.netty.timer;
 import io.netty.util.HashedWheelTimer;
 import io.netty.util.Timeout;
 import io.netty.util.Timer;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
+import org.apache.commons.lang.time.FastDateFormat;
 
 public class HashedWheelTimer_Dm_001 {
 
@@ -11,12 +13,21 @@ public class HashedWheelTimer_Dm_001 {
     Timer timer = new HashedWheelTimer();
 
     /*专门用来延迟任务*/
-    final Timeout timeoutTask = timer.newTimeout(timeout -> {
-      System.err.println("5s 后执行任务");
-    }, 5, TimeUnit.SECONDS);
+    timer.newTimeout(timeout -> {
+      info("执行任务1->会休息5s");
+      Thread.sleep(5000L);
+    }, 1, TimeUnit.SECONDS);
 
-
+    timer.newTimeout(timeout -> {
+      info("执行任务2");
+    }, 3, TimeUnit.SECONDS);
 
   }
 
+
+  private static void info(String msg) {
+    System.out.println(
+        "当前时间:" + (FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss").format(new Date())) + ":打印信息"
+            + msg);
+  }
 }
