6ae8d642eec11d9464180114344f1fa47d5d6007
carl_yu <yszlx200@163.com>
2021-02-04 15:52:24 +0800

    Simple Gateway init
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

---
+++ dm-duitang/src/main/java/com/ysz/dm/duitang/srv/support/blog/port/adapter/facade/BlogFacadeImpl.java
@@ -0,0 +1,7 @@
+package com.ysz.dm.duitang.srv.support.blog.port.adapter.facade;
+
+import com.ysz.dm.duitang.cli.support.blog.facade.BlogFacade;
+
+public class BlogFacadeImpl implements BlogFacade {
+
+}
