1b3b4a9edb6a27c280c4f80b3bce818e82491d9c
carl_yu <yszlx200@163.com>
2021-06-18 19:02:32 +0800

    netty recyler tech
    Signed-off-by: carl_yu <sizhen.yu@duitang.com>

---
+++ dm-netty/src/main/java/com/ysz/dm/netty/echo/Tmp.java
@@ -0,0 +1,12 @@
+package com.ysz.dm.netty.echo;
+
+public class Tmp {
+
+  public static void main(String[] args) {
+    double N = 24713400;
+    double n = 1114328;
+
+    System.err.println(Math.log(1 + (N - n + 0.5) / (n + 0.5)));
+  }
+
+}
