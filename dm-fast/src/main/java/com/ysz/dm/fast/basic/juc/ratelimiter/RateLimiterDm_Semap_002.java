92b20a82538be44f72b526f5663a2f1bf6380043
carl10086 <yszlx200@163.com>
2021-08-12 18:59:23 +0800

    1.  change: tst resi rate limiter with accumulation
    Signed-off-by: carl10086 <yszlx200@163.com>

--- dm-fast/src/main/java/com/ysz/dm/fast/basic/juc/ratelimiter/RateLimiterDm_Semap_002.java
+++ dm-fast/src/main/java/com/ysz/dm/fast/basic/juc/ratelimiter/RateLimiterDm_Semap_002.java
@@ -4,6 +4,9 @@ import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
 import com.google.common.util.concurrent.RateLimiter;
 import com.ysz.dm.fast.infra.tools.MixUtils;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
+import java.time.Duration;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -19,6 +22,21 @@ public class RateLimiterDm_Semap_002 {
   private Semaphore semaphore = new Semaphore(2);
 
   private RateLimiter rateLimiter = RateLimiter.create(4);
+
+
+  // Create registry
+  private RateLimiterRegistry rateLimiterRegistry = RateLimiterRegistry
+      .of(RateLimiterConfig.custom()
+          /*1s*/
+          .limitRefreshPeriod(Duration.ofSeconds(1))
+          /*允许4个*/
+          .limitForPeriod(4)
+          /*客户端等死*/
+          .timeoutDuration(Duration.ofDays(1))
+          .build());
+
+  private io.github.resilience4j.ratelimiter.RateLimiter rateLimiter2 = rateLimiterRegistry
+      .rateLimiter("rt");
 
   private AtomicInteger count = new AtomicInteger(0);
 
@@ -37,7 +55,9 @@ public class RateLimiterDm_Semap_002 {
     int threadNum = 50;
     /*2. start `*/
     for (int i = 0; i < threadNum; i++) {
-      new Thread(new Worker(this.requests, this.semaphore, this.rateLimiter, this.count)).start();
+      new Thread(
+          new Worker(this.requests, this.semaphore, this.rateLimiter, this.count, rateLimiter2))
+          .start();
     }
 
   }
@@ -54,23 +74,27 @@ public class RateLimiterDm_Semap_002 {
     private Semaphore semaphore;
     private RateLimiter rateLimiter;
     private final AtomicInteger count;
+    private final io.github.resilience4j.ratelimiter.RateLimiter rateLimiter2;
 
     private Worker(
         Meter requests,
         Semaphore semaphore,
         RateLimiter rateLimiter,
-        final AtomicInteger count) {
+        final AtomicInteger count,
+        final io.github.resilience4j.ratelimiter.RateLimiter rateLimiter2) {
       this.requests = requests;
       this.semaphore = semaphore;
       this.rateLimiter = rateLimiter;
       this.count = count;
+      this.rateLimiter2 = rateLimiter2;
     }
 
     @Override
     public void run() {
       while (true) {
         try {
-          rateLimiter.acquire();
+//          rateLimiter.acquire();
+          rateLimiter2.acquirePermission();
           MixUtils.simpleShow("进入了" + (count.incrementAndGet()));
           MixUtils.sleep(50000);
           requests.mark();
