6d65d29ae42b726deda3b4fd036e9360d739ffe1
carl10086 <sizhen.yu@duitang.com>
2021-08-25 19:00:52 +0800

    1. change: 基于数据库当前时间精度精确到微秒
    Signed-off-by: carl10086 <sizhen.yu@duitang.com>

--- dm-fast/src/main/java/Tmp.java
+++ dm-fast/src/main/java/Tmp.java
@@ -4,13 +4,7 @@ import java.util.concurrent.Future;
 public class Tmp {
 
   public static void main(String[] args) {
-    Future<Integer> submit = Executors.newFixedThreadPool(1).submit(() -> 1 / 0);
-    try {
-      submit.get();
-    } catch (Exception e) {
-      System.err.println("main thread 抓住了异常");
-      e.printStackTrace();
-    }
+    System.err.println(1);
 
   }
 }
