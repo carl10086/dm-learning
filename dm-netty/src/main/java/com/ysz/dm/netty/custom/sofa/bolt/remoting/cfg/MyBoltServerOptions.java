7eb3dd8078c6c828ddd3e871230c07cb38cb81ee
carl10086 <sizhen.yu@duitang.com>
2021-09-09 19:12:23 +0800

    1. feature: support flush
    Signed-off-by: carl10086 <sizhen.yu@duitang.com>

--- dm-netty/src/main/java/com/ysz/dm/netty/custom/sofa/bolt/remoting/cfg/MyBoltServerOptions.java
+++ dm-netty/src/main/java/com/ysz/dm/netty/custom/sofa/bolt/remoting/cfg/MyBoltServerOptions.java
@@ -24,4 +24,6 @@ public class MyBoltServerOptions {
       false);
 
 
+
+
 }
--- dm-netty/src/main/java/com/ysz/dm/netty/custom/sofa/bolt/remoting/server/MyRpcServer.java
+++ dm-netty/src/main/java/com/ysz/dm/netty/custom/sofa/bolt/remoting/server/MyRpcServer.java
@@ -1,7 +1,6 @@
 package com.ysz.dm.netty.custom.sofa.bolt.remoting.server;
 
 import com.ysz.dm.netty.custom.sofa.bolt.remoting.cfg.MyBoltGenericOptions;
-import com.ysz.dm.netty.custom.sofa.bolt.remoting.cfg.MyBoltOption;
 import com.ysz.dm.netty.custom.sofa.bolt.remoting.cfg.MyBoltServerOptions;
 import com.ysz.dm.netty.custom.sofa.bolt.remoting.core.address.MyAddressParser;
 import com.ysz.dm.netty.custom.sofa.bolt.remoting.core.address.MyAddressParserImpl;
@@ -11,10 +10,15 @@ import com.ysz.dm.netty.custom.sofa.bolt.remoting.core.connection.impl.MyRandomS
 import com.ysz.dm.netty.custom.sofa.bolt.remoting.infra.thread.MyNamedThreadFactory;
 import com.ysz.dm.netty.custom.sofa.bolt.remoting.infra.utils.MyNettyEventLoopUtils;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.channel.WriteBufferWaterMark;
 
 public class MyRpcServer extends MyAbstractRemotingServer {
 
@@ -43,6 +47,7 @@ public class MyRpcServer extends MyAbstractRemotingServer {
 
   @Override
   protected void doInit() {
+    initThenFrozenCfg();
     initAddressParser();
 
     if (getOption(MyBoltServerOptions.SERVER_MANAGE_CONNECTION_SWITCH)) {
@@ -54,11 +59,56 @@ public class MyRpcServer extends MyAbstractRemotingServer {
     }
 
     this.bootstrap = new ServerBootstrap();
-    this.bootstrap.group(boss, worker)
+    initBootstrap();
+    initWaterMark();
+    initAllocator();
+
+    final boolean idleSwitch = getOption(MyBoltGenericOptions.TCP_IDLE_SWITCH);
+    final boolean flushConsolidationSwitch = getOption(
+        MyBoltGenericOptions.NETTY_FLUSH_CONSOLIDATION);
+
+    final int idleTime = getOption(MyBoltServerOptions.TCP_SERVER_IDLE);
+    this.bootstrap.childHandler(new ChannelInitializer<>() {
+      @Override
+      protected void initChannel(final Channel channel) throws Exception {
+        final ChannelPipeline pipeline = channel.pipeline();
+        if (flushConsolidationSwitch) {
+
+        }
+      }
+    });
+
+  }
+
+  private void initAllocator() {
+    if (getOption(MyBoltGenericOptions.NETTY_BUFFER_POOLED)) {
+      this.bootstrap.option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+          .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
+    } else {
+      this.bootstrap.option(ChannelOption.ALLOCATOR, UnpooledByteBufAllocator.DEFAULT)
+          .childOption(ChannelOption.ALLOCATOR, UnpooledByteBufAllocator.DEFAULT);
+    }
+  }
+
+  private void initWaterMark() {
+    this.bootstrap.childOption(
+        ChannelOption.WRITE_BUFFER_WATER_MARK, new WriteBufferWaterMark(
+            getOption(MyBoltGenericOptions.NETTY_BUFFER_LOW_WATER_MARK),
+            getOption(MyBoltGenericOptions.NETTY_BUFFER_HIGH_WATER_MARK)
+        )
+    );
+  }
+
+  private ServerBootstrap initBootstrap() {
+    return this.bootstrap.group(boss, worker)
         .channel(MyNettyEventLoopUtils.getServerSocketChannelClass())
         .option(ChannelOption.SO_BACKLOG, getOption(MyBoltServerOptions.TCP_SO_BACKLOG))
         .option(ChannelOption.SO_REUSEADDR, getOption(MyBoltGenericOptions.TCP_SO_REUSEADDR))
+        .option(ChannelOption.TCP_NODELAY, getOption(MyBoltGenericOptions.TCP_NODELAY))
+        .option(ChannelOption.SO_KEEPALIVE, getOption(MyBoltGenericOptions.TCP_SO_KEEPALIVE));
+  }
 
+  private void initThenFrozenCfg() {
 
   }
 
