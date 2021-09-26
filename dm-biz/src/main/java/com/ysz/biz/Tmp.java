58d0346479da5eef619ee03aa3cea7aa71fbd310
carl10086 <sizhen.yu@duitang.com>
2021-09-26 13:53:36 +0800

    1. es regex valid
    Signed-off-by: carl10086 <sizhen.yu@duitang.com>

--- dm-biz/src/main/java/com/ysz/biz/Tmp.java
+++ dm-biz/src/main/java/com/ysz/biz/Tmp.java
@@ -1,106 +1,38 @@
 package com.ysz.biz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.lang3.RegExUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.PatternMatchUtils;
+
 public class Tmp {
 
-  public static void main(String[] args) {
-//    tstSql();
-    tstSql();
+  public static boolean checkEmail(String email) {
+    String regex = "\\w+@\\w+\\.[a-z]+(\\.[a-z]+)?";
+    return Pattern.matches(regex, email);
   }
 
-  private static void tstSql() {
 
-    String sql = "";
-    for (int i = 0; i < 16; i++) {
-      sql += "drop table blog_origin_" + i + ";\n";
+  private static boolean isValidEmailOrQQ(String email) {
+    if (email.length() > 50) {
+      return false;
     }
-
-    System.err.println(sql);
-
-    String createTableSql = "";
-
-    for (int i = 0; i < 16; i++) {
-      createTableSql += "create table blog_origin_" + i + "\n"
-          + "(\n"
-          + "   id bigint not null,\n"
-          + "   contentType tinyint not null,\n"
-          + "   senderId bigint not null,\n"
-          + "   albumId bigint not null,\n"
-          + "   createAt timestamp not null DEFAULT CURRENT_TIMESTAMP ,\n"
-          + "   photoId bigint not null,\n"
-          + "   officalTag varchar(255) null,\n"
-          + "   msgAsShort varchar(500) null,\n"
-          + "   ocr varchar(255) null,\n"
-          + "   ocrTextLen int   null,\n"
-          + "   status tinyint not null,\n"
-          + "   updateAt timestamp not null DEFAULT CURRENT_TIMESTAMP ,\n"
-          + "   metaVersion int default 0 not null,\n"
-          + "   sourceLink varchar(255) null,\n"
-          + "   sourceTitle varchar(255) null,\n"
-          + "   sourceType varchar(255) null,\n"
-          + "   syncOffset timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),\n"
-          + "   constraint blog_origin_0_pk primary key (id)\n"
-          + ") ENGINE = InnoDB DEFAULT CHARSET = utf8mb4; \n";
-
-      createTableSql += "create index blog_origin_createAt on blog_origin_" + i + " (createAt);\n";
+    Pattern pattern = Pattern.compile("^([1-9a-zA-Z._]+[0]?)+@[0-9a-zA-Z]+[.][a-z-]{2,10}");
+    Matcher matcher = pattern.matcher(email);
+    if (matcher.find()) {
+      return true;
     }
-
-    System.out.println(createTableSql);
-
-    createTableSql = "";
-
-    for (int i = 0; i < 16; i++) {
-      createTableSql += "create table blog_origin_msg_" + i + "\n"
-          + "(\n"
-          + "    id  bigint   not null,\n"
-          + "    msg longtext null,\n"
-          + "    constraint blog_origin_msg_pk  primary key (id)\n"
-          + ") ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;\n";
+    pattern = Pattern.compile("^[1-9][0-9]{4,10}$");
+    matcher = pattern.matcher(email);
+    if (matcher.find()) {
+      return true;
     }
+    return false;
+  }
 
-//    System.out.println(createTableSql);
-
-    createTableSql = "";
-
-    for (int i = 0; i < 16; i++) {
-      createTableSql += "create table blog_origin_audit_detail_" + i + "\n"
-          + "(\n"
-          + "    partitionId        varchar(255)                        not null,\n"
-          + "    blogId             bigint                              not null,\n"
-          + "    contentType        tinyint                             not null,\n"
-          + "    senderId           bigint                              not null,\n"
-          + "    createAt           timestamp default CURRENT_TIMESTAMP not null,\n"
-          + "    photoId            bigint                              not null,\n"
-          + "    albumId            bigint                              not null,\n"
-          + "    imgCheckSupplier   varchar(255)                        null,\n"
-          + "    hitLabel           varchar(255)                        null,\n"
-          + "    labelLevel         int                                 null,\n"
-          + "    labelRate          varchar(255)                        null,\n"
-          + "    hitWords           varchar(255)                        null,\n"
-          + "    machineStatus      tinyint                             null,\n"
-          + "    machineAt          timestamp default CURRENT_TIMESTAMP not null,\n"
-          + "    machineMsgStatus   tinyint                             null,\n"
-          + "    machinePhotoStatus tinyint                             null,\n"
-          + "    status             tinyint                             not null,\n"
-          + "    firstAt            TIMESTAMP default CURRENT_TIMESTAMP not null,\n"
-          + "    firstOp            bigint                              null,\n"
-          + "    firstStatus        tinyint                             not null,\n"
-          + "    secondAt           timestamp default CURRENT_TIMESTAMP not null,\n"
-          + "    secondOp           bigint                              null,\n"
-          + "    secondStatus       tinyint                             null,\n"
-          + "    finalStatus        tinyint                             null,\n"
-          + "    finalAuditType     tinyint                             null,\n"
-          + "    finalOperator      bigint                              null,\n"
-          + "    syncOffset         timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),\n"
-          + "    finalOperateAt     timestamp default CURRENT_TIMESTAMP not null,\n"
-          + "    lastCheckNo        bigint                              null,\n"
-          + "    lastCheckAt        timestamp                           null,\n"
-          + "    lastCheckBy        bigint                              null,\n"
-          + "    lastCheckAction    tinyint                             null,\n"
-          + "    constraint blog_origin_audit_detail_pk\n"
-          + "        primary key (blogId)\n"
-          + ") ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;\n"
-          + "\n";
-    }
-//    System.out.println(createTableSql);
+  public static void main(String[] args) {
+    final String txt = "dnjnfslkffkjkjkslioeo9edkdjfks";
+    System.out.println(checkEmail(txt));
   }
 }
