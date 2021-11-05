87249fdb68c048e5fe24a0cd8aca7bafdc6ece70
carl10086 <sizhen.yu@duitang.com>
2021-11-05 19:07:19 +0800

    script support jvm node generator
    Signed-off-by: carl10086 <sizhen.yu@duitang.com>

--- dm-fast/src/main/java/Tmp.java
+++ dm-fast/src/main/java/Tmp.java
@@ -1,9 +1,14 @@
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
 public class Tmp {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
+    final CrossThreadObj crossThreadObj = new CrossThreadObj();
+  }
+
+
+  /**
+   * 跨线程对象 , 假设有 100 个属性
+   */
+  private static class CrossThreadObj {
 
   }
 }
