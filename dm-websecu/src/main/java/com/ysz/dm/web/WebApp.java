a666f4e964c3cf9a1dd29dba0faabb5c9cf158d8
carl10086 <sizhen.yu@duitang.com>
2021-12-01 20:57:03 +0800

    spring actuaotr test
    Signed-off-by: carl10086 <sizhen.yu@duitang.com>

--- dm-websecu/src/main/java/com/ysz/dm/web/WebApp.java
+++ dm-websecu/src/main/java/com/ysz/dm/web/WebApp.java
@@ -1,12 +1,9 @@
 package com.ysz.dm.web;
 
-import com.ysz.dm.web.filter.MicroMeterFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author carl.yu
@@ -14,20 +11,13 @@ import org.springframework.context.annotation.Configuration;
  */
 @SpringBootApplication
 @Configuration
+@PropertySource(
+    value = {
+        "file:///Users/carl/Projects/IdeaProjects/dm-learning/dm-websecu/conf/a.properties"
+    }
+
+)
 public class WebApp {
-
-  @Bean
-  @ConfigurationProperties(prefix = "redis.feedrec")
-  public FeedRecRedisProperties feedRecRedisProperties() {
-    return new FeedRecRedisProperties();
-  }
-
-  @Bean
-  public FilterRegistrationBean microMeterFilter() {
-    FilterRegistrationBean registrationBean = new FilterRegistrationBean(new MicroMeterFilter());
-    registrationBean.addUrlPatterns("/*");
-    return registrationBean;
-  }
 
   public static void main(String[] args) throws Exception {
     SpringApplication.run(WebApp.class, args);
