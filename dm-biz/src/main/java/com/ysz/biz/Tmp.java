b6f8925ff0b08f083b214de65bcb631a04e4ed22
carl10086 <yszlx200@163.com>
2021-12-07 19:08:36 +0800

    move cron scripts
    Signed-off-by: carl10086 <yszlx200@163.com>

--- dm-biz/src/main/java/com/ysz/biz/Tmp.java
+++ dm-biz/src/main/java/com/ysz/biz/Tmp.java
@@ -1,2 +1,12 @@
-package com.ysz.biz;public class Tmp {
+package com.ysz.biz;
+
+public class Tmp {
+
+
+  private static class OldJobCfg {
+
+    private String owner;
+
+    private String email;
+  }
 }
