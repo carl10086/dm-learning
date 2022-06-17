dc3d983370a13b1bfc2006accf4cd038f258040d
carl10086 <yszlx200@163.com>
2022-06-17 11:03:58 +0800

    feature: graylog from warn to error

--- dm-biz/src/main/java/com/ysz/biz/Tmp.java
+++ dm-biz/src/main/java/com/ysz/biz/Tmp.java
@@ -1,17 +1,44 @@
 package com.ysz.biz;
 
-import org.apache.commons.lang3.time.FastDateFormat;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 public class Tmp {
 
 
+  public static void cal(int originInGb, int actual, int cpus) {
+
+    int originInKB = originInGb * (1024 * 1024);
+    int steal = originInKB - actual;
+    System.out
+        .printf("场景: %sg, vcpus:%s, stealInKb:%sKb, stealPerCpu:%s, stealInGb:%sGb stealPercent:%s%%\n",
+            originInGb,
+            cpus,
+            steal,
+            steal / cpus,
+            new BigDecimal(steal).divide(new BigDecimal(1024 * 1024), 2, RoundingMode.CEILING).floatValue(),
+            100f * new BigDecimal(steal).divide(new BigDecimal(actual), 4, RoundingMode.CEILING).floatValue());
+  }
+
+
   public static void main(String[] args) throws Exception {
-    final long now = FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss").parse("2022-01-24 14:56:30").getTime();
-    final long createAt = FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss").parse("2022-01-24 13:39:30").getTime();
+//    System.out.println(URLDecoder.decode("%CE%A2%D0%C5%CA%B1%C9%D0%B0%C5%C9%AF%B5%E7%D7%D3%BF%AF", "utf-8"));
+//    System.out.println(URLDecoder.decode("%CE%A2%D0%C5%CA%B1%C9%D0%B0%C5%C9%AF%B5%E7%D7%D3%BF%AF", "gbk"));
+    System.out.println(URLEncoder.encode(",什么贵哈哈哈\n\r..fdsf///", "utf-8"));
+    System.out.println(URLEncoder.encode(",什么贵哈哈哈\n\r..fdsf///", "gbk"));
 
-    long dur = now - createAt;
+  }
 
-    System.out.println(33L * now / dur);
+  private static void jdMemSteal() {
+    cal(4, 3880536, 1);
+    cal(8, 8009156, 2);
+    cal(16, 16266668, 2);
+    cal(16, 16266336, 4);
+    cal(32, 32779776, 8);
+    cal(64, 65806636, 16);
+    cal(128, 131860300, 32);
   }
 
 }
