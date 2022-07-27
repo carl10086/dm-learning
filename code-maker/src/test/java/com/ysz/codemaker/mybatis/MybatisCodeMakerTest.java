e2d304077f55676df76a35a79ce9d95709f9db80
carl10086 <yszlx200@163.com>
2022-07-27 18:50:45 +0800

    change: codemakrer

--- code-maker/src/test/java/com/ysz/codemaker/mybatis/MybatisCodeMakerTest.java
+++ code-maker/src/test/java/com/ysz/codemaker/mybatis/MybatisCodeMakerTest.java
@@ -9,7 +9,7 @@ public class MybatisCodeMakerTest {
 
   @Test
   public void execute() throws Exception {
-    String jdbcUrl = "jdbc:mysql://10.200.68.3:3306/zcwdb?zeroDateTimeBehavior=convertToNull&useSSL=false";
+    String jdbcUrl = "jdbc:mysql://10.200.68.3:3306/uactdb?zeroDateTimeBehavior=convertToNull&useSSL=false";
     String username = "adm";
     String password = "123456";
 
@@ -18,7 +18,8 @@ public class MybatisCodeMakerTest {
         .setUsername(username)
         .setPassword(password);
 
-    Cfg cfg = new Cfg().setMysql(mysqlCfg).setDatabase("zcwdb").setTableName("auth_user").setVersionColName("version");
+    Cfg cfg = new Cfg().setMysql(mysqlCfg).setDatabase("uactdb").setTableName("uact_likes_0")
+        .setVersionColName("version");
 
     Output output = new MybatisCodeMaker().execute(cfg);
 
