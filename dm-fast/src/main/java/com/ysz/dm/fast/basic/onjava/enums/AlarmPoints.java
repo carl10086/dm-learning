0b057fde36b97d0332afc38bd569d1f8776b85ca
俞思真 <carl.yu@yusizhendeMac-mini.local>
2022-09-26 22:59:29 +0800

    basic enum

---
+++ dm-fast/src/main/java/com/ysz/dm/fast/basic/onjava/enums/AlarmPoints.java
@@ -0,0 +1,14 @@
+package com.ysz.dm.fast.basic.onjava.enums;
+
+/**
+ * <pre>
+ * class desc here
+ * </pre>
+ *
+ * @author carl.yu
+ * @createAt 2022/9/26
+ **/
+public enum AlarmPoints {
+  STAIR1, STAIR2, LOBBY, OFFICE1, OFFICE2, OFFICE3,
+  OFFICE4, BATHROOM, UTILITY, KITCHEN;
+}
