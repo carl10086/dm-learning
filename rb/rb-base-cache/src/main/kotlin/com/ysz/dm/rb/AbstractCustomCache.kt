3a6c2bf5d51fc14fa16e55e111decee94a3cb6cb
俞思真 <carl@7.170.169.192.host.secureserver.net>
2022-11-16 19:18:46 +0800

    integration with cache

--- rb/rb-base-cache/src/main/kotlin/com/ysz/dm/rb/AbstractCustomCache.kt
+++ rb/rb-base-cache/src/main/kotlin/com/ysz/dm/rb/AbstractCustomCache.kt
@@ -1,11 +1,56 @@
 package com.ysz.dm.rb
 
+import org.slf4j.LoggerFactory
+import java.util.function.Function
+
 /**
  * @author carl
  * @create 2022-11-15 3:51 PM
  **/
-abstract class AbstractCustomCache<K, V>(
-    /*common cfg for all implementation*/
-    val cfg: CustomCacheCfg
+abstract class AbstractCustomCache<K, V>(/*common cfg for all implementation*/
+                                         val cfg: CustomCacheCfg
 ) : CustomCache<K, V> {
+
+    override fun get(
+        k: K, sorLoader: Function<K, V>, penetrationProtect: Boolean, fallback: Function<K, V>?
+    ): V? {
+        return try {/*1. success direct return*/
+            doGet(k) ?: let {
+                /*2. cache miss load from sor*/
+                return sorLoader.apply(k)?.let {
+                    doPut(k, it)
+                    return it
+                }
+            }
+        } catch (e: Exception) {
+            log.error("error occurred when get key, k:{}", k, e)
+            /*3. error happened, if fallback is null then wrap with custom exception*/
+            fallback?.apply(k) ?: throw CustomCacheException("load failed , key :${k}", e)
+        }
+    }
+
+    override fun multiGet(keys: Collection<K>, loader: Function<Collection<K>, Map<K, V>>, fallback: Function<K, V>?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun rm(k: K): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun rmAll(keys: Collection<K>): Map<K, Boolean> {
+        return super.rmAll(keys)
+    }
+
+    /**
+     * get from cache
+     */
+    abstract fun doGet(k: K): V?;
+
+
+    abstract fun doPut(k: K, v: V);
+
+
+    companion object {
+        private val log = LoggerFactory.getLogger(AbstractCustomCache::class.java)
+    }
 }
--- rb/rb-base-cache/src/main/kotlin/com/ysz/dm/rb/lettuce/CustomCacheLettuceImpl.kt
+++ rb/rb-base-cache/src/main/kotlin/com/ysz/dm/rb/lettuce/CustomCacheLettuceImpl.kt
@@ -3,36 +3,21 @@ package com.ysz.dm.rb.lettuce
 import com.ysz.dm.rb.AbstractCustomCache
 import com.ysz.dm.rb.CustomCacheCfg
 import io.lettuce.core.api.sync.RedisCommands
-import java.util.function.Function
 
 /**
  * @author carl
  * @create 2022-11-15 4:36 PM
  **/
 class CustomCacheLettuceImpl<K, V>(
-    cfg: CustomCacheCfg,
-    /*this connection maybe shared in different cache*/
-    private val conn: CustomLettuceConnection<K, V>
+    cfg: CustomCacheCfg,/*this connection maybe shared in different cache*/
+    conn: CustomLettuceConnection<K, V>
 ) : AbstractCustomCache<K, V>(cfg) {
 
     private val sync: RedisCommands<K, V> = conn.conn.sync()
 
-
-    override fun get(k: K, sorLoader: Function<K, V>, penetrationProtect: Boolean, fallback: Function<K, V>?): V? {
-        TODO("Not yet implemented")
-    }
-
-    fun close() {
-        /*true force to close current commands*/
+    fun close() {/*true force to close current commands*/
         this.sync.shutdown(false)
     }
 
-    override fun multiGet(keys: Collection<K>, loader: Function<Collection<K>, Map<K, V>>, fallback: Function<K, V>?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun rm(k: K): Boolean {
-        TODO("Not yet implemented")
-    }
 
 }
