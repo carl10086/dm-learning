ac7f83893cc0a56ba069f22965da72d49062a18c
俞思真 <carl.yu@yusizhendeMac-mini.local>
2022-11-17 22:55:29 +0800

    remove useless framework

--- dm-biz/src/main/java/com/ysz/biz/ansj/trie/Basic_Trie_Dm001.java
+++
@@ -1,8 +0,0 @@
-package com.ysz.biz.ansj.trie;
-
-public class Basic_Trie_Dm001 {
-
-  public static void main(String[] args) throws Exception {
-
-  }
-}
--- dm-biz/src/main/java/com/ysz/biz/httpua/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.ysz.biz.httpua;
