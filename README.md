0910af3e39f6df2450c38e1b2b761a19a09b8c16
carl10086 <yszlx200@163.com>
2023-01-28 01:31:42 +0800

    leeCode1664

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 As more and more language are using in our workloads .
 
 - support template code for modern language such as `python` , `scala`, `kotlin`, `golang`, `typescript` and so on .
+
+
+
+## 2. sub projects
+
+
+- dm-kt: kotlin projects
+- ansible: ansible projects .
+- systemd:
---
+++ dm-kt/dm-lib/src/test/kotlin/com/ysz/dm/lib/leecode/LeeCode1664.kt
@@ -0,0 +1,68 @@
+package com.ysz.dm.lib.leecode
+
+/**
+ * @author carl
+ * @since 2023-01-28 12:47 AM
+ **/
+class LeeCode1664 {
+    companion object {
+        fun waysToMakeFair(nums: IntArray): Int {
+            var evenSum = 0
+            var oddSum = 0
+            nums.forEachIndexed { index, i ->
+                if (index % 2 == 0) {
+                    evenSum += i
+                } else {
+                    oddSum += i
+                }
+            }
+            println("origin: ${nums.contentToString()}")
+            println("evenSum:$evenSum, oddSum:$oddSum")
+
+
+            var res = 0
+
+            var oddNew: Int
+            var evenNew: Int
+
+            var oddBefore = 0
+            var evenBefore = 0
+
+            var oddAfter: Int
+            var evenAfter: Int
+
+
+            nums.forEachIndexed { index, i ->
+                oddAfter = oddSum - oddBefore
+                evenAfter = evenSum - evenBefore
+                if (index % 2 == 0) {
+                    // 偶数
+                    oddNew = oddBefore + evenAfter - i
+                    evenNew = evenBefore + oddAfter
+
+                    println("index:${index}, i:${i}, evenBefore: $evenBefore, evenAfter: $evenAfter , oddBefore:${oddBefore} , oddAfter: $oddAfter")
+
+                    evenBefore += i
+                } else {
+                    oddNew = oddBefore + evenAfter
+                    evenNew = evenBefore + oddAfter - i
+
+                    println("index:${index}, i:${i}, evenBefore: $evenBefore, evenAfter: $evenAfter , oddBefore:${oddBefore} , oddAfter: $oddAfter")
+                    oddBefore += i
+                }
+
+
+
+                if (oddNew == evenNew) res++
+
+            }
+
+
+            return res
+        }
+    }
+}
+
+fun main() {
+    LeeCode1664.waysToMakeFair(intArrayOf(2, 1, 6, 4))
+}
