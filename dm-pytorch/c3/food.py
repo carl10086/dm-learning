0d7641a26c61b327383a79cf6960801e86d5a373
carl10086 <yszlx200@163.com>
2023-02-20 23:37:15 +0800

    feat:  torch augmation

--- dm-pytorch/c3/food.py
+++ dm-pytorch/c3/food.py
@@ -58,7 +58,8 @@ class FoodDataset(Dataset):
     def __getitem__(self, idx):
         fname = self.files[idx]
         im = Image.open(fname)
-        im = self.transform(im)
+        if self.transform:
+            im = self.transform(im)
         # im = self.data[idx]
         try:
             label = int(fname.split("/")[-1].split("_")[0])
@@ -113,136 +114,139 @@ class Classifier(nn.Module):
         return self.fc(out)
 
 
-batch_size = 64
-_dataset_dir = "/tmp/dataset/food11"
-# Construct datasets.
-# The argument "loader" tells how torchvision reads the data.
-train_set = FoodDataset(os.path.join(_dataset_dir, "training"), tfm=train_tfm)
-train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=True)
-valid_set = FoodDataset(os.path.join(_dataset_dir, "validation"), tfm=test_tfm)
-valid_loader = DataLoader(valid_set, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=True)
+if __name__ == '__main__':
+    batch_size = 64
+    _dataset_dir = "/tmp/dataset/food11"
+    # Construct datasets.
+    # The argument "loader" tells how torchvision reads the data.
+    train_set = FoodDataset(os.path.join(_dataset_dir, "training"), tfm=train_tfm)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=True)
+    valid_set = FoodDataset(os.path.join(_dataset_dir, "validation"), tfm=test_tfm)
+    valid_loader = DataLoader(valid_set, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=True)
 
-# "cuda" only when GPUs are available.
-device = "cuda" if torch.cuda.is_available() else "cpu"
+    # "cuda" only when GPUs are available.
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# The number of training epochs and patience.
-n_epochs = 3
-patience = 300  # If no improvement in 'patience' epochs, early stop
+    # The number of training epochs and patience.
+    n_epochs = 3
+    patience = 300  # If no improvement in 'patience' epochs, early stop
 
-# Initialize a model, and put it on the device specified.
-model = Classifier().to(device)
+    # Initialize a model, and put it on the device specified.
+    model = Classifier().to(device)
 
-# For the classification task, we use cross-entropy as the measurement of performance.
-criterion = nn.CrossEntropyLoss()
+    # For the classification task, we use cross-entropy as the measurement of performance.
+    criterion = nn.CrossEntropyLoss()
 
-# Initialize optimizer, you may fine-tune some hyperparameters such as learning rate on your own.
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0003, weight_decay=1e-5)
+    # Initialize optimizer, you may fine-tune some hyperparameters such as learning rate on your own.
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0003, weight_decay=1e-5)
 
-# Initialize trackers, these are not parameters and should not be changed
-stale = 0
-best_acc = 0
+    # Initialize trackers, these are not parameters and should not be changed
+    stale = 0
+    best_acc = 0
 
-for epoch in range(n_epochs):
+    for epoch in range(n_epochs):
 
-    # ---------- Training ----------
-    # Make sure the model is in train mode before training.
-    model.train()
+        # ---------- Training ----------
+        # Make sure the model is in train mode before training.
+        model.train()
 
-    # These are used to record information in training.
-    train_loss = []
-    train_accs = []
+        # These are used to record information in training.
+        train_loss = []
+        train_accs = []
 
-    for batch in tqdm(train_loader):
-        # A batch consists of image data and corresponding labels.
-        imgs, labels = batch
-        # imgs = imgs.half()
-        # print(imgs.shape,labels.shape)
+        for batch in tqdm(train_loader):
+            # A batch consists of image data and corresponding labels.
+            imgs, labels = batch
+            # imgs = imgs.half()
+            # print(imgs.shape,labels.shape)
 
-        # Forward the data. (Make sure data and model are on the same device.)
-        logits = model(imgs.to(device))
-
-        # Calculate the cross-entropy loss.
-        # We don't need to apply softmax before computing cross-entropy as it is done automatically.
-        loss = criterion(logits, labels.to(device))
-
-        # Gradients stored in the parameters in the previous step should be cleared out first.
-        optimizer.zero_grad()
-
-        # Compute the gradients for parameters.
-        loss.backward()
-
-        # Clip the gradient norms for stable training.
-        grad_norm = nn.utils.clip_grad_norm_(model.parameters(), max_norm=10)
-
-        # Update the parameters with computed gradients.
-        optimizer.step()
-
-        # Compute the accuracy for current batch.
-        acc = (logits.argmax(dim=-1) == labels.to(device)).float().mean()
-
-        # Record the loss and accuracy.
-        train_loss.append(loss.item())
-        train_accs.append(acc)
-
-    train_loss = sum(train_loss) / len(train_loss)
-    train_acc = sum(train_accs) / len(train_accs)
-
-    # Print the information.
-    print(f"[ Train | {epoch + 1:03d}/{n_epochs:03d} ] loss = {train_loss:.5f}, acc = {train_acc:.5f}")
-
-    # ---------- Validation ----------
-    # Make sure the model is in eval mode so that some modules like dropout are disabled and work normally.
-    model.eval()
-
-    # These are used to record information in validation.
-    valid_loss = []
-    valid_accs = []
-
-    # Iterate the validation set by batches.
-    for batch in tqdm(valid_loader):
-        # A batch consists of image data and corresponding labels.
-        imgs, labels = batch
-        # imgs = imgs.half()
-
-        # We don't need gradient in validation.
-        # Using torch.no_grad() accelerates the forward process.
-        with torch.no_grad():
+            # Forward the data. (Make sure data and model are on the same device.)
             logits = model(imgs.to(device))
 
-        # We can still compute the loss (but not the gradient).
-        loss = criterion(logits, labels.to(device))
+            # Calculate the cross-entropy loss.
+            # We don't need to apply softmax before computing cross-entropy as it is done automatically.
+            loss = criterion(logits, labels.to(device))
 
-        # Compute the accuracy for current batch.
-        acc = (logits.argmax(dim=-1) == labels.to(device)).float().mean()
+            # Gradients stored in the parameters in the previous step should be cleared out first.
+            optimizer.zero_grad()
 
-        # Record the loss and accuracy.
-        valid_loss.append(loss.item())
-        valid_accs.append(acc)
-        # break
+            # Compute the gradients for parameters.
+            loss.backward()
 
-    # The average loss and accuracy for entire validation set is the average of the recorded values.
-    valid_loss = sum(valid_loss) / len(valid_loss)
-    valid_acc = sum(valid_accs) / len(valid_accs)
+            # Clip the gradient norms for stable training.
+            grad_norm = nn.utils.clip_grad_norm_(model.parameters(), max_norm=10)
 
-    # Print the information.
-    print(f"[ Valid | {epoch + 1:03d}/{n_epochs:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}")
+            # Update the parameters with computed gradients.
+            optimizer.step()
 
-    # update logs
-    if valid_acc > best_acc:
-        with open(f"./{_exp_name}_log.txt", "a"):
-            print(f"[ Valid | {epoch + 1:03d}/{n_epochs:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f} -> best")
-    else:
-        with open(f"./{_exp_name}_log.txt", "a"):
-            print(f"[ Valid | {epoch + 1:03d}/{n_epochs:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}")
+            # Compute the accuracy for current batch.
+            acc = (logits.argmax(dim=-1) == labels.to(device)).float().mean()
 
-    # save models
-    if valid_acc > best_acc:
-        print(f"Best model found at epoch {epoch}, saving model")
-        torch.save(model.state_dict(), f"{_exp_name}_best.ckpt")  # only save best to prevent output memory exceed error
-        best_acc = valid_acc
-        stale = 0
-    else:
-        stale += 1
-        if stale > patience:
-            print(f"No improvment {patience} consecutive epochs, early stopping")
-            break
+            # Record the loss and accuracy.
+            train_loss.append(loss.item())
+            train_accs.append(acc)
+
+        train_loss = sum(train_loss) / len(train_loss)
+        train_acc = sum(train_accs) / len(train_accs)
+
+        # Print the information.
+        print(f"[ Train | {epoch + 1:03d}/{n_epochs:03d} ] loss = {train_loss:.5f}, acc = {train_acc:.5f}")
+
+        # ---------- Validation ----------
+        # Make sure the model is in eval mode so that some modules like dropout are disabled and work normally.
+        model.eval()
+
+        # These are used to record information in validation.
+        valid_loss = []
+        valid_accs = []
+
+        # Iterate the validation set by batches.
+        for batch in tqdm(valid_loader):
+            # A batch consists of image data and corresponding labels.
+            imgs, labels = batch
+            # imgs = imgs.half()
+
+            # We don't need gradient in validation.
+            # Using torch.no_grad() accelerates the forward process.
+            with torch.no_grad():
+                logits = model(imgs.to(device))
+
+            # We can still compute the loss (but not the gradient).
+            loss = criterion(logits, labels.to(device))
+
+            # Compute the accuracy for current batch.
+            acc = (logits.argmax(dim=-1) == labels.to(device)).float().mean()
+
+            # Record the loss and accuracy.
+            valid_loss.append(loss.item())
+            valid_accs.append(acc)
+            # break
+
+        # The average loss and accuracy for entire validation set is the average of the recorded values.
+        valid_loss = sum(valid_loss) / len(valid_loss)
+        valid_acc = sum(valid_accs) / len(valid_accs)
+
+        # Print the information.
+        print(f"[ Valid | {epoch + 1:03d}/{n_epochs:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}")
+
+        # update logs
+        if valid_acc > best_acc:
+            with open(f"./{_exp_name}_log.txt", "a"):
+                print(
+                    f"[ Valid | {epoch + 1:03d}/{n_epochs:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f} -> best")
+        else:
+            with open(f"./{_exp_name}_log.txt", "a"):
+                print(f"[ Valid | {epoch + 1:03d}/{n_epochs:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}")
+
+        # save models
+        if valid_acc > best_acc:
+            print(f"Best model found at epoch {epoch}, saving model")
+            torch.save(model.state_dict(),
+                       f"{_exp_name}_best.ckpt")  # only save best to prevent output memory exceed error
+            best_acc = valid_acc
+            stale = 0
+        else:
+            stale += 1
+            if stale > patience:
+                print(f"No improvment {patience} consecutive epochs, early stopping")
+                break
