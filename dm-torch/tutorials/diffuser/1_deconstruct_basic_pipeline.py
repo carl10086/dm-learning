c320f2bbe098fe88f68ca1f897a42c21397987a5
俞思真 <carl@154.170.169.192.host.secureserver.net>
2023-06-14 15:36:54 +0800

    feat: kandinsky

--- dm-torch/tutorials/diffuser/1_deconstruct_basic_pipeline.py
+++ dm-torch/tutorials/diffuser/1_deconstruct_basic_pipeline.py
@@ -24,6 +24,7 @@ def gen_by_steps():
     global sample_size
     scheduler = DDPMScheduler.from_pretrained("google/ddpm-cat-256")
     model = UNet2DModel.from_pretrained("google/ddpm-cat-256").to("cuda")
+    # print(model.config)
     # 1. Set the number of timesteps to run the denoising process for
     scheduler.set_timesteps(50)
     # 2. 随机化初始化一个噪音 当作输入图片的代表, 符合模型 shape 要求即可
@@ -47,27 +48,23 @@ def gen_by_steps():
 
 SD_MODEL = "runwayml/stable-diffusion-v1-5"
 
-if __name__ == '__main__':
+
+def gen_with_sd():
+    global unet
     # AutoEncoder 用来实现和 latent  的交互
     vae = AutoencoderKL.from_pretrained(SD_MODEL, subfolder="vae")
-
     # CLIP tokenizer 用来 分词 把文字转为 token 向量
     tokenizer = CLIPTokenizer.from_pretrained(SD_MODEL, subfolder="tokenizer")
-
     # CLIP text 文本 embedding 模型
     text_encoder = CLIPTextModel.from_pretrained(SD_MODEL, subfolder="text_encoder")
-
     # UNet2DConditionModel 扩散模型
     unet = UNet2DConditionModel.from_pretrained(SD_MODEL, subfolder="unet")
-
     # 调度器
     scheduler = UniPCMultistepScheduler.from_pretrained(SD_MODEL, subfolder="scheduler")
-
     torch_device = "cuda"
     vae.to(torch_device)
     text_encoder.to(torch_device)
     unet.to(torch_device)
-
     prompt = ["a photograph of an astronaut riding a horse"]
     height = 512  # default height of Stable Diffusion
     width = 512  # default width of Stable Diffusion
@@ -75,35 +72,26 @@ if __name__ == '__main__':
     guidance_scale = 7.5  # Scale for classifier-free guidance
     generator = torch.manual_seed(0)  # Seed generator to create the inital latent noise
     batch_size = len(prompt)
-
     text_input = tokenizer(
         prompt, padding="max_length", max_length=tokenizer.model_max_length, truncation=True, return_tensors="pt"
     )
-
     with torch.no_grad():
         text_embeddings = text_encoder(text_input.input_ids.to(torch_device))[0]
-
     # You’ll also need to generate the unconditional ƒtext embeddings which are the embeddings for the padding token
     max_length = text_input.input_ids.shape[-1]
     uncond_input = tokenizer([""] * batch_size, padding="max_length", max_length=max_length, return_tensors="pt")
     uncond_embeddings = text_encoder(uncond_input.input_ids.to(torch_device))[0]
-
     text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
-
     # generate some initial random noise as a starting point for the diffusion process
     latents = torch.randn(
         (batch_size, unet.in_channels, height // 8, width // 8),
         generator=generator,
     )
     latents = latents.to(torch_device)
-
     # sigma, the noise scale value, which is required for improved schedulers like UniPCMultistepScheduler:
     latents = latents * scheduler.init_noise_sigma
-
     from tqdm.auto import tqdm
-
     scheduler.set_timesteps(num_inference_steps)
-
     for t in tqdm(scheduler.timesteps):
         # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
         latent_model_input = torch.cat([latents] * 2)
@@ -120,14 +108,17 @@ if __name__ == '__main__':
 
         # compute the previous noisy sample x_t -> x_t-1
         latents = scheduler.step(noise_pred, t, latents).prev_sample
-
     # scale and decode the image latents with vae
     latents = 1 / 0.18215 * latents
     with torch.no_grad():
         image = vae.decode(latents).sample
-
     # 4. 把最后的 一步的 noisy_sample 作为最终的输出
     image = (image / 2 + 0.5).clamp(0, 1)
     image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
     # images = (image * 255).round().astype("uint8")
     show_np(image[0])
+
+
+if __name__ == '__main__':
+    # gen_with_sd()
+    gen_by_steps()
